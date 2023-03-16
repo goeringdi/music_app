b0dfa447ac0ba5dbc13d246bafb02458c59653cf
Diana Goering <goeringdi@gmail.com>
2023-03-16 12:46:03 +0100

    kurs

---
+++ src/components/Playlist/Playlist/playlist.test.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Playlist from './Playlist'
+import { setupApiStore } from '../../../utils/reduxQueryTest-utils'
+import { musicApi } from '../../../api/musicApi'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+import { render, screen } from '@testing-library/react'
+
+// имитируем данные
+const allTracks = [
+  {
+    id: 8,
+    name: 'Chase',
+    author: 'Alexander Nakarada',
+    release_date: '2005-06-11',
+    genre: 'Классическая музыка',
+  },
+]
+
+// Описываем endpoint-ы, которые хотим замокировать
+export const handlers = [
+  rest.get('https://painassasin.online/catalog/track/all', (req, res, ctx) => {
+    return res(ctx.json(allTracks))
+  }),
+]
+
+const server = setupServer(...handlers)
+
+const storeRef = setupApiStore(musicApi)
+
+describe('MusicList feature', () => {
+  beforeAll(() => server.listen())
+
+
+  afterEach(() => server.resetHandlers())
+
+
+  afterAll(() => server.close())
+
+
+  it('should show playlist', async () => {
+  
+    render(<Playlist />, { wrapper: storeRef.wrapper })
+    // screen.debug()
+
+   // Ищем в списке текст Chase, который совпадает с названием трека
+    expect(await screen.findByText('Chase')).toBeInTheDocument()
+  })
+});
