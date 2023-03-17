25442f4c3f15c76b38738249381db778fa66e6a2
Diana Goering <goeringdi@gmail.com>
2023-03-17 19:39:46 +0100

    courseNew

--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -47,6 +47,28 @@ export const Input = styled.input`
         color: rgb(123 120 120);
     }
 `
+export const FormButton = styled.button`
+    width: 278px;
+    height: 52px;
+    font-family: 'StratosSkyeng', serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: -0.05px;
+    color: white;
+    background-color: rgba(88, 14, 162);
+    border-radius: 6px;
+    border: 0;
+    cursor: pointer;
+    margin: 10px;
+    :hover {
+        background-color: rgb(64 8 120);
+    }
+    :active {
+        background-color: rgb(43 10 77);
+    }
+`
 
 export const Button = styled.button`
     width: 278px;
